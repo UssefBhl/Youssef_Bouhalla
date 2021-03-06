a45ae9a32ff6a5d474f3fa97b00b4974b4b6018d
UssefBhl <ybhl83@gmail.com>
2021-03-06 20:20:03 +0100

    finished login.php form in html

---
+++ PHP-COMMENT-PROJECT/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container" role="main">
+        <form action="comments.php" method="get">
+            <input type="text" id="username" name="username">
+            <input type="password" id="pass" name="pass">
+            <button type="submit" name="login" id="login">LogIn</button>
+        </form>
+    </div>
+</body>
+</html>
